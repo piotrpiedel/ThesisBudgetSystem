783555add72cf7556c40e0f1ad7c0f543ae8b168
piotrpiedel <piotrpiedel96@gmail.com>
2019-10-26 01:53:42 +0200

    current version is working;

--- app/src/main/java/piedel/piotr/thesis/ui/fragment/operation/operationaddlist/OperationAddListPresenter.kt
+++ app/src/main/java/piedel/piotr/thesis/ui/fragment/operation/operationaddlist/OperationAddListPresenter.kt
@@ -1,11 +1,15 @@
 package piedel.piotr.thesis.ui.fragment.operation.operationaddlist
 
+import io.reactivex.CompletableObserver
 import io.reactivex.disposables.Disposable
+import piedel.piotr.thesis.data.model.operation.Operation
 import piedel.piotr.thesis.data.model.operation.OperationRepository
+import piedel.piotr.thesis.data.model.operation.OperationSelectable
 import piedel.piotr.thesis.injection.scopes.ConfigPersistent
 import piedel.piotr.thesis.ui.base.BasePresenter
 import piedel.piotr.thesis.ui.fragment.operation.operationaddlist.OperationAddListContract.OperationView
 import piedel.piotr.thesis.ui.fragment.operation.operationaddlist.OperationAddListContract.PresenterContract
+import timber.log.Timber
 import javax.inject.Inject
 
 
@@ -21,4 +25,26 @@ constructor(private val operationRepository: OperationRepository) : BasePresente
         view?.setAdapter()
     }
 
+    fun saveSelectedOperations(operationList: ArrayList<OperationSelectable>) {
+        val selectedOperationList: Array<Operation> = operationList
+                .filter { operationSelectable -> operationSelectable.selected }
+                .map { operationSelectable -> operationSelectable.operation }
+                .toTypedArray()
+
+        operationRepository.insertOperation(*selectedOperationList)
+                .subscribe(object : CompletableObserver {
+                    override fun onComplete() {
+                        Timber.d("oncomplete fucker")
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+                        Timber.d("onSubscribe fucker")
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Timber.e(e, "onError")
+                    }
+
+                })
+    }
 }
