186d25628c4599d0afa2f093542e49cb712c3b85
piotrpiedel <piotrpiedel96@gmail.com>
2019-10-25 22:19:24 +0200

    highliting is owkring slecatle recyclewviewer;

--- app/src/main/java/piedel/piotr/thesis/ui/fragment/operation/operationaddlist/OperationAddListFragment.kt
+++ app/src/main/java/piedel/piotr/thesis/ui/fragment/operation/operationaddlist/OperationAddListFragment.kt
@@ -5,21 +5,18 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import androidx.recyclerview.selection.SelectionPredicates
-import androidx.recyclerview.selection.SelectionTracker
-import androidx.recyclerview.selection.StableIdKeyProvider
-import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import butterknife.BindView
 import piedel.piotr.thesis.R
 import piedel.piotr.thesis.data.model.operation.Operation
+import piedel.piotr.thesis.data.model.operation.OperationSelectable
 import piedel.piotr.thesis.ui.base.BaseFragment
 import piedel.piotr.thesis.util.showToast
 import timber.log.Timber
 import javax.inject.Inject
 
-class OperationAddListFragment : BaseFragment(), OperationAddListContract.OperationView, OperationAddListAdapter.OperationAdapterListener {
+class OperationAddListFragment : BaseFragment(), OperationAddListContract.OperationView {
 
     @Inject
     lateinit var operationAddListPresenter: OperationAddListPresenter
@@ -30,7 +27,7 @@ class OperationAddListFragment : BaseFragment(), OperationAddListContract.Operat
     @BindView(R.id.recycler_view_operations)
     lateinit var operationsRecyclerView: RecyclerView
 
-    private var operationArrayList: ArrayList<Operation> = arrayListOf()
+    private var operationArrayList: ArrayList<OperationSelectable> = arrayListOf()
 
     override val toolbarTitle: String
         get() = context?.getString(R.string.operations_list).toString()
@@ -53,11 +50,10 @@ class OperationAddListFragment : BaseFragment(), OperationAddListContract.Operat
         super.onViewCreated(view, savedInstanceState)
         operationArrayList = arguments?.getParcelableArrayList(OPERATION_LIST_PASSED)
                 ?: arrayListOf() // temp
-        operationArrayList.add(Operation("asfasf"))
-        operationArrayList.add(Operation("asffas"))
-        operationArrayList.add(Operation("asfas2eqw"))
-        operationArrayList.add(Operation("asfas232"))
-        operationArrayList.add(Operation("asfasfasfas232"))
+        operationArrayList.add(OperationSelectable(Operation("asfasf")))
+        operationArrayList.add(OperationSelectable(Operation("asfas2eqw")))
+        operationArrayList.add(OperationSelectable(Operation("asfas232")))
+        operationArrayList.add(OperationSelectable(Operation("asfasfasfas232")))
         operationAddListPresenter.initFragment()
     }
 
@@ -88,12 +84,8 @@ class OperationAddListFragment : BaseFragment(), OperationAddListContract.Operat
 //        operationAddListAdapter.operationWithCategoryList = operationArrayList.toMutableList()
         operationAddListAdapter.notifyDataSetChanged()
         Timber.d("setAdapter()")
-//        operationAddListAdapter.updateListOfOperations(operationArrayList.toList())
+        operationAddListAdapter.updateListOfOperations(operationArrayList.toList())
 //        operationAddListAdapter.setClickListener(this)
-    }
-
-    override fun onOperationItemClicked() {
-
     }
 
     override fun showError(throwable: Throwable) {
@@ -104,7 +96,7 @@ class OperationAddListFragment : BaseFragment(), OperationAddListContract.Operat
         const val FRAGMENT_TAG: String = "OperationAddListFragment"
         const val OPERATION_LIST_PASSED: String = "OPERATION_KEY"
 
-        fun newInstance(operationList: ArrayList<Operation>): OperationAddListFragment {
+        fun newInstance(operationList: ArrayList<OperationSelectable>): OperationAddListFragment {
             val operationAddListFragment = OperationAddListFragment()
             val args = Bundle().apply {
                 putParcelableArrayList(OPERATION_LIST_PASSED, operationList)
